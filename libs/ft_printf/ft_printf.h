d3112d46729a43e0b60f336c1641309ce780d0f8
Skiams <antoine.hayon@gmail.com>
2024-07-02 18:01:53 +0200

    premier push test

---
+++ libs/ft_printf/ft_printf.h
@@ -0,0 +1,25 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_printf.h                                        :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: ahayon <marvin@42.fr>                      +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2023/06/16 10:12:04 by ahayon            #+#    #+#             */
+/*   Updated: 2023/06/16 11:10:31 by ahayon           ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#ifndef FT_PRINTF_H
+
+# define FT_PRINTF_H
+
+# include <stdarg.h>
+# include <unistd.h>
+
+int	ft_printf(const char *format, ...);
+int	ft_print_char(char c);
+int	ft_print_str(char *str);
+int	ft_print_hexmaj(long n, int base);
+int	ft_print_digit(long n, int base);
+#endif
