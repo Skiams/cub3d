3534270ab0ef559424210dde38047cb8913ac4cf
Skiams <antoine.hayon@gmail.com>
2024-07-02 18:40:52 +0200

    retrait libft

--- cub3d.h
+++ cub3d.h
@@ -6,10 +6,21 @@
 /*   By: ahayon <ahayon@student.42.fr>              +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/07/02 15:19:42 by ahayon            #+#    #+#             */
-/*   Updated: 2024/07/02 15:21:14 by ahayon           ###   ########.fr       */
+/*   Updated: 2024/07/02 18:37:50 by ahayon           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #ifndef CUB3D_H
 # define CUB3D_H
 
+# include "./mlx/mlx.h"
+# include "libs/libft/libft.h"
+# include "libs/ft_printf/ft_printf.h"
+# include <X11/keysym.h>
+# include <fcntl.h>
+# include <stdio.h>
+# include <stdlib.h>
+# include <unistd.h>
+
+
+#endif
