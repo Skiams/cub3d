cc1c2fa04ecc3653da18e12656e6986f1b1c3954
Skiams <skiam@DESKTOP-LBO7IOP>
2024-07-09 18:02:06 +0200

    Makefile + mini gnl pour map

--- errors.c
+++ errors.c
@@ -1,28 +1,14 @@
 /* ************************************************************************** */
 /*                                                                            */
 /*                                                        :::      ::::::::   */
-/*   main.c                                             :+:      :+:    :+:   */
+/*   errors.c                                           :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
 /*   By: skiam <skiam@student.42.fr>                +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
-/*   Created: 2024/07/02 18:49:43 by ahayon            #+#    #+#             */
-/*   Updated: 2024/07/09 17:59:46 by skiam            ###   ########.fr       */
+/*   Created: 2024/07/08 15:34:25 by skiam             #+#    #+#             */
+/*   Updated: 2024/07/08 15:34:35 by skiam            ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "cub3d.h"
 
-static bool	parsing(char *argv, t_data *data)
-{
-	check_format_cub(argv)
-}
-
-int	main(int argc, char **argv)
-{
-	static t_data	data = {0};
-	
-	if (argc != 2)
-		ft_printf("Error: Wrong argument\nPlease enter a .cub file as argument");
-	if (!parsing(argv[1]))
-		return (1);
-}
--- parsing.c
+++ parsing.c
@@ -3,14 +3,37 @@
 /*                                                        :::      ::::::::   */
 /*   parsing.c                                          :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: ahayon <ahayon@student.42.fr>              +#+  +:+       +#+        */
+/*   By: skiam <skiam@student.42.fr>                +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/07/02 15:19:20 by ahayon            #+#    #+#             */
-/*   Updated: 2024/07/02 19:00:37 by ahayon           ###   ########.fr       */
+/*   Updated: 2024/07/09 17:50:55 by skiam            ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "cub3d.h"
+
+char	*mini_gnl(int fd)
+{
+	char	*map;
+	int		reads;
+	char	buffer[1024];
+
+	reads = 1;
+	map = NULL;
+	while (reads > 0)
+	{
+		reads = read(fd, buffer, 1024);
+		if (reads == -1)
+		{
+			ft_printf("Error\nMap cannot be read\n");
+			free(map);
+			exit(1);
+		}
+		buffer[reads] = '\0';
+		map = ft_strjoin(map, buffer);
+	}
+	return (map);
+}
 
 void	check_format(char *str)
 {
@@ -28,8 +51,3 @@ void	check_format(char *str)
 	if (str[i - 4] != '.')
 		return (ft_printf("Error\nWrong map format\n"), exit(1));
 }
-
-// bool	parsing()
-// {
-	
-// }
