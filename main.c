fabe215cb2175cf553221a9b1291153dd3ba5d1f
Skiams <skiam@DESKTOP-LBO7IOP>
2024-07-12 02:00:14 +0200

    continue parsing

--- main.c
+++ main.c
@@ -3,10 +3,10 @@
 /*                                                        :::      ::::::::   */
 /*   main.c                                             :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: ahayon <ahayon@student.42.fr>              +#+  +:+       +#+        */
+/*   By: skiam <skiam@student.42.fr>                +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/07/02 18:49:43 by ahayon            #+#    #+#             */
-/*   Updated: 2024/07/10 18:18:13 by ahayon           ###   ########.fr       */
+/*   Updated: 2024/07/11 20:49:05 by skiam            ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -17,7 +17,7 @@ int	main(int argc, char **argv)
 	static t_data	data = {0};
 	
 	if (argc != 2)
-		ft_printf("Error: Wrong argument\nPlease enter a .cub file as argument");
+		ft_printf("Error: Wrong argument\nPlease enter one .cub file as argument");
 	if (!parsing(argv[1], &data))
 		return (printf("problem parsing"), 1);
 	return (0);
--- parsing.c
+++ parsing.c
@@ -3,50 +3,75 @@
 /*                                                        :::      ::::::::   */
 /*   parsing.c                                          :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: ahayon <ahayon@student.42.fr>              +#+  +:+       +#+        */
+/*   By: skiam <skiam@student.42.fr>                +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/07/02 15:19:20 by ahayon            #+#    #+#             */
-/*   Updated: 2024/07/10 18:29:57 by ahayon           ###   ########.fr       */
+/*   Updated: 2024/07/12 01:35:26 by skiam            ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "cub3d.h"
 
-// static bool	assign_texture(t_data *data, char *path, int code)
-// {
-// 	if (code == NORTH)
-// 	{
-// 		data->img_north = mlx_xpm_file_to_image(data->mlx, path, )
-// 	}
-		
-// 	return (true);
-// }
+static bool	assign_texture(t_data *data, char *path, int code)
+{
+	if (code == NORTH)
+	{
+		data->img_north = mlx_xpm_file_to_image(data->mlx, path, 200, 200);
+		if (data->img_north == NULL)
+			return (ft_printf("Error\nTexture can't be opened\n"), false);
+	}
+	else if (code == SOUTH)
+	{
+		data->img_south = mlx_xpm_file_to_image(data->mlx, path, 200, 200);
+		if (data->img_south == NULL)
+			return (ft_printf("Error\nTexture can't be opened\n"), false);
+	}
+	else if (code == EAST)
+	{
+		data->img_east = mlx_xpm_file_to_image(data->mlx, path, 200, 200);
+		if (data->img_east == NULL)
+			return (ft_printf("Error\nTexture can't be opened\n"), false);
+	}
+	else if (code == WEST)
+	{
+		data->img_west = mlx_xpm_file_to_image(data->mlx, path, 200, 200);
+		if (data->img_west == NULL)
+			return (ft_printf("Error\nTexture can't be opened\n"), false);
+	}
+	return (true);
+}
+
+static bool get_colors(t_data *data, char *map_line, int *i, int code)
+{
+	
+}
 
 static bool	get_texture(t_data *data, char *map_line, int *i, int code)
 {
 	char	*texture;
 	int		j;
 	
-	data->tc = 0;
-	code = 0;
 	(*i) += 2;
 	if (map_line[*i] && (map_line[*i] != 32 && map_line[*i] != 9))
-		return (ft_printf("Error\nInvalid "))
+		return (ft_printf("Error\nInvalid texture format\n"), false);
 	while (map_line[*i] && (map_line[*i] == 32 || map_line[*i] == 9 || map_line[*i] == '\n')) 
 		(*i)++;
 	j = (*i);
 	while (map_line[*i] && (map_line[*i] != 32 && map_line[*i] != 9 && map_line[*i] != '\n'))
 		(*i)++;
 	printf("i = %d, j = %d\n", (*i), j);
-	texture = ft_substr(map_line, map_line[j], ((*i) - j));
+	texture = ft_substr(map_line, j, ((*i) - j));
 	if (!texture)
 		return (ft_printf("Error\nMemory allocation problem\n"), false);
 	printf("texture = %s\n", texture);
-	// if (!assign_texture(data, texture, code))
-	// 	return (false);
+	if (is_path_dir(texture) || !check_format(texture, 2))
+		return (false);
+	if (!assign_texture(data, texture, code))
+		return (false);
 	free(texture);
 	return (true);
 }
+
 static bool	check_param(t_data *data, char *map_line)
 {
 	int	i;
@@ -56,14 +81,23 @@ static bool	check_param(t_data *data, char *map_line)
 	{
 		while (map_line[i] == 32 || map_line[i] == 9 || map_line[i] == '\n')
 			i++;
-		if (map_line[i] == 'N' && map_line[i + 1] == 'O' && (!get_texture(data, map_line, &i, NORTH)))
+		if (map_line[i] == 'N' && map_line[i + 1] == 'O' && data->img_north == NULL
+		&& (!get_texture(data, map_line, &i, NORTH)))
 			return (false);			
-		else if (map_line[i] == 'S' && map_line[i + 1] == 'O' && (!get_texture(data, map_line, &i, SOUTH)))
+		else if (map_line[i] == 'S' && map_line[i + 1] == 'O' && data->img_south == NULL
+		&& (!get_texture(data, map_line, &i, SOUTH)))
 			return (false);
-		else if (map_line[i] == 'W' && map_line[i + 1] == 'E' && (!get_texture(data, map_line, &i, WEST)))
+		else if (map_line[i] == 'W' && map_line[i + 1] == 'E' && data->img_west == NULL
+		&& (!get_texture(data, map_line, &i, WEST)))
 			return (false);
-		else if (map_line[i] == 'E' && map_line[i + 1] == 'A' && (!get_texture(data, map_line, &i, EAST)))
+		else if (map_line[i] == 'E' && map_line[i + 1] == 'A' && data->img_east == NULL
+		&& (!get_texture(data, map_line, &i, EAST)))
 			return (false);
+		else if (map_line[i] == 'F' && !get_colors(data, map_line, &i, FLOOR))
+			return (false);
+		else if (map_line[i] == 'C' && !get_colors(data, map_line, &i, CEILING))
+			return (false);
+		//check pour voir si tout a bien ete pris en compte avant la map
 	}
 	return (true);
 }
@@ -71,7 +105,7 @@ static bool	check_param(t_data *data, char *map_line)
 
 bool	parsing(char *argv, t_data *data)
 {
-	if (check_format(argv, 1) == false || is_path_dir(argv) == true)
+	if (!check_format(argv, 1) || is_path_dir(argv))
 		return (ft_printf("Error\nInvalid map format\n", false));
 	data->map_line = mini_gnl(open(argv, O_RDONLY));
 	printf("map_line1 = %s\n", data->map_line);
