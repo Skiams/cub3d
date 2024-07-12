184da83154360a5863ee2a99d428766002addf36
Skiams <antoine.hayon@gmail.com>
2024-07-12 18:54:44 +0200

    continue parsing texture + colors presque OK

--- parsing_utils.c
+++ parsing_utils.c
@@ -3,14 +3,35 @@
 /*                                                        :::      ::::::::   */
 /*   parsing_utils.c                                    :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: skiam <skiam@student.42.fr>                +#+  +:+       +#+        */
+/*   By: ahayon <ahayon@student.42.fr>              +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/07/10 14:25:03 by ahayon            #+#    #+#             */
-/*   Updated: 2024/07/11 17:41:12 by skiam            ###   ########.fr       */
+/*   Updated: 2024/07/12 17:46:31 by ahayon           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "cub3d.h"
+
+int	ft_atoi_cub(const char *nptr)
+{
+	int	i;
+	int	signe;
+	int	res;
+
+	i = 0;
+	res = 0;
+	signe = 1;
+	if (nptr[i] < 48 || nptr[i] > 57)
+		return (-1);
+	while (nptr[i] >= 48 && nptr[i] <= 57)
+	{
+		res = (res * 10) + (nptr[i] - '0');
+		i++;
+	}
+	if (nptr[i] && (nptr[i] < 48 || nptr[i] > 57))
+		return (-1);
+	return (res * signe);
+}
 
 bool	is_path_dir(char *path)
 {
@@ -56,13 +77,13 @@ bool	check_format(char *str, int code)
 	{
 		if ((str[i - 1] != 'b') || (str[i - 2] != 'u')
 		|| (str[i - 3] != 'c') || (str[i - 4] != '.'))
-			return (false);
+			return (ft_printf("Error\nInvalid map format\n"), false);
 	}
 	else if (code == 2)
 	{
 		if ((str[i - 1] != 'm') || (str[i - 2] != 'p')
 		|| (str[i - 3] != 'x') || (str[i - 4] != '.'))
-			return (false);
+			return (ft_printf("Error\nInvalid texture format\n"),false);
 	}
 	return (true);
 }
